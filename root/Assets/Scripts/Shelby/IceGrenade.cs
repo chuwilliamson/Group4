6266dd4e54872405244cfc4dd37696bef0fb48dc
HicksusTheGreat <slanden05@gmail.com>
2015-05-21 17:08:06 -0500

    database functionality
    added some db stuff

--- root/Assets/Scripts/Shelby/IceGrenade.cs
+++ root/Assets/Scripts/Shelby/IceGrenade.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class IceGrenade : Grenade {
+public class IceGrenade : Grenade 
+{
+    public void Drop()
+    {
+        inventory.items.Remove(gameObject);
+        //inventory.grenades_ice--;
 
+        transform.parent = null;
+    }
  
 }
