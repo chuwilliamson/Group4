953f4305652ea23abf5958eb099e7945da04e1f7
coringuyen <tran.nguyen@students.aie.edu.au>
2015-05-22 16:56:07 +1100

    tran working on the enemy behavior, added an enemy scene a navmesh and scripts

--- root/Assets/Scripts/Tran/EnemyStates.cs
+++ root/Assets/Scripts/Tran/EnemyStates.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class EnemyStates : MonoBehaviour {
 
-    
+    NavMeshAgent enemy;
+    public GameObject player;
+
     public enum States { Idle, Patroll, Chase };
     public States enemyStates = States.Idle;
     public Transform[] points;
+    public float delay;
 
+    public float timer;
     private int destPoint = 0;
-
-    NavMeshAgent enemy;
-    public GameObject player;
+   
 
 	void Start () 
     {
@@ -30,21 +32,35 @@ public class EnemyStates : MonoBehaviour {
 
 	void Update () 
     {
-
+        timer += Time.deltaTime;
 	    switch(enemyStates)
         {
             case States.Idle:
+
                 enemy.velocity = new Vector3(0, 0, 0);
+
+                if (Vector3.Distance(enemy.transform.position, player.transform.position) <= enemy.radius)
+                { enemyStates = States.Chase; }
+
+                if(timer > delay)
+                { enemyStates = States.Patroll; }
                 break;
 
             case States.Patroll:
-                if(enemy.remainingDistance < 0.5f)
+
+                if(enemy.remainingDistance < 1)
                 { GoToNextPoint(); }
+
+                if(Vector3.Distance(enemy.transform.position, player.transform.position) <= enemy.radius)
+                { enemyStates = States.Chase; }
                 break;
 
             case States.Chase:
                
                 enemy.destination = player.transform.position;
+
+                if (Vector3.Distance(enemy.transform.position, player.transform.position) > 6)
+                { timer = 0; enemyStates = States.Idle; }
                 break;
         }
 
