4b879b31a5d0c582658e69085a6749399ed5b78e
deadshotversion2 <bernard.castello@yahoo.com>
2015-06-03 16:32:21 -0500

    FPS Targeting Prototype
    Set up a PlayerAttack, EnemyHealth, and Reticule scripts and you are
    able to kill a capsule in 3 hits.

---
+++ root/Assets/Scripts/Bernard/EnemyHealth.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour 
+{
+    public int startingHealth = 3;
+    public int currentHealth;
+
+    Animator anim;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+
+    void Awake()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        currentHealth = startingHealth;
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        capsuleCollider.isTrigger = true;
+
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(int damage, Vector3 hitPoint)
+    {
+        if (isDead)
+            return;
+
+        currentHealth -= damage;
+
+        if(currentHealth<=0)
+        {
+            Death();
+        }
+    }
+
+
+
+}
