d90d4c903313288ffe1ca5cce18fb838a77e80c3
Zac-King <zacharyking18@yahoo.com>
2015-06-03 13:09:56 -0500

    merge

--- root/Assets/Editor/GameManagerEditor.cs
+++ root/Assets/Editor/GameManagerEditor.cs
@@ -26,12 +26,12 @@ public class GameManagerEditor : Editor
     //}
 
 
-    public static int scene = 0;
-    [MenuItem("Zac Tools/Load level\"level_00")]
-    static public void LoadLevelZero()
-    {
-        Application.LoadLevel("level_00");
-    }
+    //public static int scene = 0;
+    //[MenuItem("Zac Tools/Load level\"level_00")]
+    //static public void LoadLevelZero()
+    //{
+    //    Application.LoadLevel("level_00");
+    //}
 
     [MenuItem("Zac Tools/Populate levels")]
     static public void PopulateLevels()
@@ -42,53 +42,56 @@ public class GameManagerEditor : Editor
         FileInfo[] info = dir.GetFiles("*.*");
         foreach(FileInfo f in info)
         {
-            
             //grab up to a point 
             //then take that point away
             //if the path contains .unity
             if (f.ToString().Contains(".unity"))
             {
-                //if the path contains .meta
+                //if the path does not contain .meta
                 if (!f.ToString().Contains(".meta"))             
                 {
+                    //Whats left is a scene file
+                    string ft = f.ToString();
+                    ft = ft.Remove(' ');
                     Debug.Log(f);
+                    Debug.Log(ft);
                 }
      
             }
         }
     }
-    [MenuItem("Zac Tools/Make Scene : \"Level_\"<int>")]
-    static public void SceneMake_Level()
-    {
+    //[MenuItem("Zac Tools/Make Scene : \"Level_\"<int>")]
+    //static public void SceneMake_Level()
+    //{
         
-        if (GameManager.instance.Levels.Contains("Level_" + scene))
-        {
-            scene += 1;
-            GameManagerEditor.SceneMake_Level();
-        }
-        // generate Scene named Level_<scene>
-        else
-        {
-            GameManager.instance.Levels.Add("Level_" + scene);
-            //EditorApplication.NewScene();
-            //EditorApplication.SaveScene("Assests/Scenes/Level_" + scene);
-        }
-        scene = 0;
-    }
+    //    if (GameManager.instance.Levels.Contains("Level_" + scene))
+    //    {
+    //        scene += 1;
+    //        GameManagerEditor.SceneMake_Level();
+    //    }
+    //    // generate Scene named Level_<scene>
+    //    else
+    //    {
+    //        GameManager.instance.Levels.Add("Level_" + scene);
+    //        //EditorApplication.NewScene();
+    //        //EditorApplication.SaveScene("Assests/Scenes/Level_" + scene);
+    //    }
+    //    scene = 0;
+    //}
 
-    [MenuItem("Zac Tools/Make Scene : \"Menu_\"<int>")]
-    static public void SceneMake_Menu()
-    {
-        if (GameManager.instance.Levels.Contains("Menu_" + scene))
-        {
-            scene += 1;
-            GameManagerEditor.SceneMake_Menu();
-        }
-        // generate Scene named Menu_<scene>
-        else
-            GameManager.instance.Levels.Add("Menu_" + scene);
-        scene = 0;
-    }
+    //[MenuItem("Zac Tools/Make Scene : \"Menu_\"<int>")]
+    //static public void SceneMake_Menu()
+    //{
+    //    if (GameManager.instance.Levels.Contains("Menu_" + scene))
+    //    {
+    //        scene += 1;
+    //        GameManagerEditor.SceneMake_Menu();
+    //    }
+    //    // generate Scene named Menu_<scene>
+    //    else
+    //        GameManager.instance.Levels.Add("Menu_" + scene);
+    //    scene = 0;
+    //}
 
 
     
