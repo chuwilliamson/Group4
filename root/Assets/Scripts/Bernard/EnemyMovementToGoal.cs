ca75eb86f98e95456b774a07eb5c867557af9912
deadshotversion2 <bernard.castello@yahoo.com>
2015-06-04 15:46:04 -0500

    Enemy Tracking Prototype

---
+++ root/Assets/Scripts/Bernard/EnemyMovementToGoal.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovementToGoal : MonoBehaviour 
+{
+    Transform goal;
+
+    GoalHealth goalHealth;
+
+    bool GoalInRange;
+
+    NavMeshAgent nav;
+
+	void Awake () 
+    {
+        goal = GameObject.FindGameObjectWithTag("Target").transform;
+        goalHealth = goal.GetComponent<GoalHealth>();
+        nav = GetComponent<NavMeshAgent>();
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject==goal)
+        {
+            GoalInRange = true;
+        }
+    }
+
+	void Update () 
+    {
+	    if(goalHealth.currentHealth>0)
+        {
+            nav.SetDestination(goal.position);
+        }
+
+        else
+        {
+            nav.enabled = false;
+        }
+	}
+}
