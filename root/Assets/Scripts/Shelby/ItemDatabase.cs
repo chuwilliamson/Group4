5092f89ed57b79c79a9eb0bda3c34cc8155dc54b
brokenCarry <thebrokekiller@yahoo.com>
2015-06-05 14:29:55 -0500

    Intergration of all the stuff
    So much headache

--- root/Assets/Scripts/Shelby/ItemDatabase.cs
+++ root/Assets/Scripts/Shelby/ItemDatabase.cs
@@ -5,13 +5,5 @@ using System.Collections.Generic;
 public class ItemDatabase : Database
 {
     public int scraps, scraps_special;
-    private int goalScraps = 100;
 
-    void Update()
-    {
-        if(scraps >= goalScraps)
-        {
-            
-        }
-    }
 }
