d56d55990329abf5601aa14386c06eb92607f896
brokenCarry <thebrokekiller@yahoo.com>
2015-06-12 10:08:22 -0500

    Getting states for turrets set up
    lets get moving

--- root/Assets/Scripts/Dylan/TurretScripts/TurretActions.cs
+++ root/Assets/Scripts/Dylan/TurretScripts/TurretActions.cs
@@ -1,8 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TurretActions : MonoBehaviour, TActions 
+public class TurretActions : MonoBehaviour, TActions
 {
-    
+    protected static TurretActions t_instance;
+    private TurretFSM t_fsm;
 
+
+    void Awake()
+    {
+        t_fsm = new TurretFSM();
+        t_instance = this;
+    }
+
+    public void Reload()
+    {
+        if (t_fsm.TurretActions["reload"] == true)
+            print("Im Reloading");
+    }
+
+    public void OnTriggerStay(Collider c)
+    {
+
+    }
+
+    public void tState(TurretState state)
+    {
+        t_fsm.t_ChangeState(state);
+    }
+
+    public static TurretActions TInstance
+    {
+        get
+        {
+            return t_instance;
+        }
+    }
 }
---
+++ root/Assets/Scripts/Dylan/TurretScripts/TurretStats.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretStats : Stats
+{
+    public float rotationSpeed = 5.0f;
+    public float reloadSpeed = 5.0f;
+    public float rateOfFire = .25f;
+
+    public int maxAmmo = 100;
+    public int currentAmmo = 100;
+    public int damage = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_MaxHealth = 100;
+        m_Health = m_MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
