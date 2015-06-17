83c45fc3f9726e333716cba250b7930849cd7e30
coringuyen <tran.nguyen@students.aie.edu.au>
2015-06-17 13:43:02 +1100

    audio

--- root/Assets/Scripts/_Matthew/FixExit.cs
+++ root/Assets/Scripts/_Matthew/FixExit.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class FixExit : MonoBehaviour {
 
+    public AudioClip GameOver;
+
 	// Use this for initialization
 	void Start () {
         HUDManager.instance.SetState("start",  false);
         HUDManager.instance.SetState("panel",  false);
         HUDManager.instance.SetState("menu",   false);
         HUDManager.instance.SetState("finish", true);
+        AudioManager.instance.PlayAudio(GameOver);
+
         print("fix exit");
 	
 	}
