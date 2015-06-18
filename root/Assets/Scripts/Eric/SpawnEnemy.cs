873e92743542b9778cadb224e1b74f02d66c8264
Eric <Eric.Mouledoux@students.aie.edu.au>
2015-06-18 12:59:07 -0500

    Arena now has nav mesh

--- root/Assets/Scripts/Eric/SpawnEnemy.cs
+++ root/Assets/Scripts/Eric/SpawnEnemy.cs
@@ -20,9 +20,9 @@ public class SpawnEnemy : MonoBehaviour
 
         if (m_Timer >= SpawnDelay)
         {
+            m_Timer = 0;
             int pos = Random.Range(0, spawnPoints.Length);
             Instantiate(enemy, spawnPoints[pos].position, transform.rotation);
-            enemy.transform.localScale *= Random.Range(1f, 2f);
         }
 	}
 }
